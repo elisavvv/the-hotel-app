26e5adf52505a53740c77731420546a110c4062e
elisavvv <elizaveta15@yandex.ru>
2025-06-07 00:53:53 +0500

    Итоговый вариант

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors} from '@angular/common/http';
 import { provideNativeDateAdapter } from '@angular/material/core';
-import { authInterceptor } from './auth/auth.interceptor'; // Импорт интерсептора
+import { authInterceptor } from './auth/auth.interceptor'; 
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { GlobalErrorHandler } from './core/handlers/global-error-handler';
 import { errorInterceptor } from '././core/interceptors/error.interceptor';
 import { MatDialogModule } from '@angular/material/dialog'; 
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,7 +25,10 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     importProvidersFrom(MatDialogModule,
       MatDatepickerModule,
-      MatSnackBarModule
+      MatSnackBarModule,
+      MatCardModule,
+      MatButtonModule,
+      MatIconModule
     ),
   ]
 };
--- src/app/contact-topic-selector/contact-topic-selector.component.ts
+++ src/app/contact-topic-selector/contact-topic-selector.component.ts
@@ -20,7 +20,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 })
 
 export class ContactTopicSelectorComponent implements ControlValueAccessor {
-  @Input() topics: Array<{ value: string; label: string }> = []; // Добавьте этот Input
+  @Input() topics: Array<{ value: string; label: string }> = []; 
   value: string = '';
   disabled = false;
   onChange: (value: string) => void = () => {};
